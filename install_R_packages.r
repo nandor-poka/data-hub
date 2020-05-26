653d36f793dc1d6213fbe631110a8b73682c41af
np <np@gerhardt.io>
2020-05-26 11:13:38 +0200

    added jsonlite package to R

--- install_R_packages.r
+++ install_R_packages.r
@@ -2,4 +2,4 @@
 #!/usr/bin/env r
 install.packages("BiocManager",repos = "http://cran.us.r-project.org")
 BiocManager::install(version = "3.10")
-
+install.packages("jsonlite",repos = "http://cran.us.r-project.org")
