7c7e0cd8d4ed4c4503798dbef0cde80a84f8f881
np <np@gerhardt.io>
2020-06-19 19:06:11 +0200

    removed ggplot package from python added plots and related package to julia added plotly JL extension

--- install_R_packages.r
+++ install_R_packages.r
@@ -3,3 +3,6 @@
 install.packages("BiocManager",repos = "http://cran.us.r-project.org")
 BiocManager::install(version = "3.10")
 install.packages("jsonlite",repos = "http://cran.us.r-project.org")
+install.packages("rbokeh", repos = "http://cran.us.r-project.org")
+install.packages("ggplot2", repos = "http://cran.us.r-project.org")
+install.packages("lattice", repos = "http://cran.us.r-project.org")
